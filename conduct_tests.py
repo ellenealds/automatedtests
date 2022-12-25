6dfdfa8d2b713be7885644c8cd9cbf55816a8bc8
ellenealds <elle.neal@bpaquality.co.uk>
2022-12-25 09:19:10 +0000

    Update conduct_tests.py

--- conduct_tests.py
+++ conduct_tests.py
@@ -66,38 +66,38 @@ def run_all_tests(df):
   
   # Iterate over each combination of two questions
   for i, question1 in enumerate(questions):
-    try: 
-        for j, question2 in enumerate(questions[i+1:]):
-        # Check the data type of the questions
-        question1_type = df[question1].dtype
-        question2_type = df[question2].dtype
-
-        # Conduct the appropriate test based on the data types
-        if question1_type == 'float' and question2_type == 'float':
-            corr, p_value = stats.pearsonr(df[question1], df[question2])
-            test = 'Pearson correlation'
-            statistic = corr
-        elif question1_type == 'object' and question2_type == 'object':
-            contingency_table = pd.crosstab(df[question1], df[question2])
-            chi2, p, dof, expected = stats.chi2_contingency(contingency_table)
-            test = 'Chi-square test of independence'
-            statistic = chi2
-        elif question1_type == 'float' and question2_type == 'object':
-            t, p_value = stats.ttest_ind(df[df[question2] == df[question2].unique()[0]][question1], df[df[question2] == df[question2].unique()[1]][question1])
-            test = 'Independent t-test'
-            statistic = t
-        elif question1_type == 'object' and question2_type == 'float':
-            f, p_value = stats.f_oneway(df[df[question1] == df[question1].unique()[0]][question2], df[df[question1] == df[question1].unique()[1]][question2])
-            test = 'One-way ANOVA'
-            statistic = f
-        else:
+    for j, question2 in enumerate(questions[i+1:]):
+        try:
+            # Check the data type of the questions
+            question1_type = df[question1].dtype
+            question2_type = df[question2].dtype
+            
+            # Conduct the appropriate test based on the data types
+            if question1_type == 'float' and question2_type == 'float':
+                corr, p_value = stats.pearsonr(df[question1], df[question2])
+                test = 'Pearson correlation'
+                statistic = corr
+            elif question1_type == 'object' and question2_type == 'object':
+                contingency_table = pd.crosstab(df[question1], df[question2])
+                chi2, p, dof, expected = stats.chi2_contingency(contingency_table)
+                test = 'Chi-square test of independence'
+                statistic = chi2
+            elif question1_type == 'float' and question2_type == 'object':
+                t, p_value = stats.ttest_ind(df[df[question2] == df[question2].unique()[0]][question1], df[df[question2] == df[question2].unique()[1]][question1])
+                test = 'Independent t-test'
+                statistic = t
+            elif question1_type == 'object' and question2_type == 'float':
+                f, p_value = stats.f_oneway(df[df[question1] == df[question1].unique()[0]][question2], df[df[question1] == df[question1].unique()[1]][question2])
+                test = 'One-way ANOVA'
+                statistic = f
+        except:
             continue
 
         # Add the results to the DataFrame
         results = results.append({'question1': question1, 'question2': question2, 'test': test, 'statistic': statistic, 'p_value': p_value, 'count': df[question2].count(), 'mean': df[question2].mean(), 'sum': df[question2].sum()}, ignore_index=True)
 
     return results
-
+    
 
 
 
